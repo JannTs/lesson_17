c18470c3d75f7d174b1b2e4483488c2eb430aa10
Evgenii Tsapko <yevgeni.tsapko@gmail.com>
2023-08-10 02:19:54 +0200

    homework17 Task1.java

---
+++ src/homework17/Phone.java
@@ -0,0 +1,39 @@
+package homework17;
+/**
+ * Создайте класс Phone, который содержит поля number, model и weight.
+ * Создайте три экземпляра этого класса. Выведите на консоль значения их полей.
+ * Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего.
+ * Выводит на консоль сообщение “Звонит {name}”.
+ * Метод getNumber – возвращает номер телефона. Вызвать эти методы для каждого из объектов.
+ * Добавить конструктор в класс Phone, который принимает на вход три параметра для инициализации полей класса
+ * - number, model и weight.
+ * Добавить конструктор, который принимает на вход два параметра для инициализации полей класса - number, model.
+ * Добавить конструктор без параметров.
+ * Вызвать из конструктора с тремя параметрами конструктор с двумя.
+ * Добавьте перегруженный метод receiveCall, который принимает два параметра - имя звонящего
+ * и номер телефона звонящего.
+ * Вызвать этот метод.
+ */
+
+public class Phone {
+    String number;
+    String model;
+    double weight;
+    public Phone(String number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+    public Phone(String number, String model) {
+        this(number, model, 0.0);
+    }
+    public Phone() {
+        this("", "");
+    }
+    void receiveCall(String callerName) {
+        System.out.println("Caller " + callerName);
+    }
+    void receiveCall(String callerName, String callerID) {
+        System.out.println("Call " + callerName + " from number " + callerID);
+    }
+}
---
+++ src/homework17/Task0.java
@@ -0,0 +1,20 @@
+package homework17;
+
+public class Task0 {
+    public static void main(String[] args) {
+
+            Phone phone1 = new Phone("243456767", "Google Pixel", 0.2);
+            Phone phone2 = new Phone("787867676", "Motorola");
+            Phone phone3 = new Phone();
+            System.out.println("Телефон первый: ");
+            phone1.receiveCall("Kateryna");
+            System.out.println("Телефон второй: ");
+            phone2.receiveCall("Evgenii", "243-456-767");
+            System.out.println("Телефон третий: ");
+            phone3.receiveCall("Ivan");
+        }
+
+
+
+
+}
